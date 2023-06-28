00d46a2a0b95608453c59acdf261d044f44d479c
JoelCann <joelcann12@gmail.com>
2023-06-28 14:35:44 +0000

    task 2 corrections

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    add = 0
-    for i in set(my_list):
-        add += i
-    return add
+    addition = 0
+    for a in set(my_list):
+        add += a
+    return addition
