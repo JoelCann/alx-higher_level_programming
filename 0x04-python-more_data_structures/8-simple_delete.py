3542940359130eb6caaaf006e4a9857a4f3b983e
JoelCann <joelcann12@gmail.com>
2023-06-28 14:49:16 +0000

    task 7,8,9 corrections

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def simple_delete(my_dict, key=""):
-    if key in my_dict:
-        del my_dict[key]
-    return my_dict
+def simple_delete(the_dict, key=""):
+    if key in the_dict:
+        del the_dict[key]
+    return the_dict
