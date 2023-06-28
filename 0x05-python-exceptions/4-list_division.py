a8a6a7388caee81e5a1b694339bdadfaf3bf140a
JoelCann <joelcann12@gmail.com>
2023-06-28 11:26:47 +0000

    corrections on task 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
-    for i in range(list_length):
+    for q in range(list_length):
         try:
-            result = my_list_1[i] / my_list_2[i]
+            result = my_list_1[q] / my_list_2[q]
         except (ValueError, TypeError):
             print("wrong type")
             result = 0
