53080a80760bdc3e79fd56718c60c068365569d0
JoelCann <joelcann12@gmail.com>
2023-10-02 14:31:51 +0000

    Task 2

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-This is the "2-matrix_divided" module.
-The 2-matrix_divided module supplies one function, matrix_divided(matrix, div).
+The "2-matrix_divided" module.
+The 2-matrix_divided module contains the function matrix_divided(matrix, div).
 """
 
 
 def matrix_divided(matrix, div):
-    """Divides all elements in the matrix by div"""
+    """Divides every element in matrix by div"""
     if type(matrix) is not list:
         raise TypeError(
             "matrix must be a matrix (list of lists) of integers/floats")
